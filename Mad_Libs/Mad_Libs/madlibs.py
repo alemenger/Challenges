83579ceaf640e9813b40510c6a40fed76d94fd3c
alemenger <76016352+alemenger@users.noreply.github.com>
2022-06-25 23:27:31 -0400

    Add files via upload

---
+++ Mad_Libs/Mad_Libs/madlibs.py
@@ -0,0 +1,14 @@
+
+name = input(" Enter your name:")
+color = input(" Enter your favorite color:")
+dish = input(" Enter your favorite meal:")
+age = int(input(" Enter your age:"))
+
+                    
+
+message = (f" This is the story of {name}.\n" 
+           f" Who was born {age} years ago. \n"
+           f" The  favorite color of {name} is the {color},\n"  
+           f" and {name}'s favorite meal is: {dish}. It is so tasty!\n")
+
+print("\n\n" + message)
